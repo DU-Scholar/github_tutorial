e96643bb947efc9ab0d3f52d1bd8acf8cf473fab
kazuki-komori <komokazu.nankai@gmail.com>
2020-04-23 13:46:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## gitを用いた共同開発のフロー
+
+Gitを用いた共同開発のフローを、実際に手を動かしながら学べる講座です。
+
+タイポや修正依頼はPRベースで出してもらえると助かります。
+
+## setup
+
+```
+npm i -g vuepress
+```
+
+```
+npm install
+```
+
+## build server
+
+```
+npm run dev 
+```
+
+### author
+
+小森 一輝 
---
+++ books/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: "GitHub入門講座",
+    locales: {
+        '/': {
+            lang: 'ja',
+        },
+    },
+    themeConfig: {
+        description: "GitHubを用いた共同開発のフロー",
+        nav: [],
+        sidebar: [
+            '/',
+            ['/1.gitを用いた共同開発のフロー/', "gitを用いた共同開発のフロー"],
+            ['/2.はじめてのGithub/', "はじめてのGithub"],
+            ['/3.初めてのプロジェクト/', "初めてのプロジェクト"],
+        ],
+        repo: 'DU-scholar/github_tutorial',
+        repoLabel: 'Github',
+        editLinks: true,
+        editLinkText: 'ページに不明点や誤字等があれば、Github にて修正を提案してください！',
+    }
+}
